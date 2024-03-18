9ebf177c5984cf1554f2ea3f629694079e1671d8
yeabsirahaile <yeabsirahaile01@gmail.com>
2024-03-18 17:24:52 +0300

    UI-3

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,24 +2,20 @@ import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="bg-blue-500 text-white h-screen w-48 p-4">
-      <h1 className="text-2xl font-semibold mb-6">CV Maker</h1>
+    <div className="bg-blue-500 text-white h-screen w-40">
+      <h1 className="text-2xl font-semibold mb-6 p-4">CV Maker</h1>
 
-      <div className=" flex flex-col gap-y-1">
-        <NavLink to="/" className=" p-1 text-lg hover:bg-blue-600/70 ">
+      <div className=" flex flex-col gap-y-1 pl-4">
+        <NavLink to="/" className=" p-1 text-md hover:bg-blue-600/70 ">
           Home
         </NavLink>
 
-        <NavLink to="/allcv" className=" p-1 text-lg hover:bg-blue-600/70">
-          All CV
+        <NavLink to="/createcv" className=" p-1 text-md hover:bg-blue-600/70">
+          Create CV
         </NavLink>
 
-        <NavLink to="/createcv" className=" p-1 text-lg hover:bg-blue-600/70">
-          ADD CV
-        </NavLink>
-
-        <NavLink to="/preview" className=" p-1 text-lg hover:bg-blue-600/70">
-          Preview trial
+        <NavLink to="/allcv" className=" p-1 text-md hover:bg-blue-600/70">
+          All CVs
         </NavLink>
       </div>
     </div>
