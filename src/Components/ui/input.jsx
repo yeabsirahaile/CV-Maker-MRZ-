511dc4c5f5472bc95e06bde897926eb19a52bfd0
yeabsirahaile <yeabsirahaile01@gmail.com>
2024-03-20 01:04:53 +0300

    UI-3

--- src/Components/ui/input.jsx
+++ src/Components/ui/input.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "../../lib/utils";
 
 const Input = React.forwardRef(
   ({ className, type, value, nam, ...props }, ref) => {
