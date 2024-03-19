7e6ab8fe5a6acda4f94005b906ea6e1854269a3c
yeabsirahaile <yeabsirahaile01@gmail.com>
2024-03-20 00:54:03 +0300

    UI-3

--- src/Components/AddClient/Forms/Buttons.jsx
+++ src/Components/AddClient/Forms/Buttons.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "../../ui/button";
 import { useFormContext } from "../../../context/FormContext";
 import { useNavigate } from "react-router-dom";
 
